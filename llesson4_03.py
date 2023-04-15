33fe8849fa34f44c194882633cd8e58a07cbf046
David0933997594 <126947010+David0933997594@users.noreply.github.com>
2023-04-15 13:45:43 +0800

    Create llesson4_03.py

---
+++ llesson4_03.py
@@ -0,0 +1,18 @@
+
+import gpiozero
+from time import sleep
+
+mcp3008_light = gpiozero.MCP3008(channel=7)
+mcp3008_temperature = gpiozero.MCP3008(channel=6)
+buzzer = gpiozero.Buzzer(25)
+
+while(True):    
+    lightValue = round(mcp3008_light.value*1000)                      #整數
+    temperature = round(mcp3008_temperature.value*3.3*100,ndigits=2)  #溫度小數後二位
+    print(temperature)
+
+    if lightValue < 40:
+        buzzer.on()
+    else:
+        buzzer.off()
+    sleep(1)
